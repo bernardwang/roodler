b0f324b3349386570dd3b6e89a27be521a2777b6
bw-vbnm <bwang19@illinois.edu>
2013-11-21 14:15:04 -0600

    put existing static work into flask html
    also changed file names and js formatting

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,77 +1,78 @@
 
 
 if(window.addEventListener) {
-window.addEventListener('load', function () {
+  window.addEventListener('load', function () {
 	
-	var canvas, context;
-	var clearButton = document.getElementById["clear"];
+	  var canvas, context;
+	  var clearButton = document.getElementById["clear"];
 	
-	function init () {
-  		// Find the canvas element.
-    	canvas = document.getElementById('canvasApp');
-    	if (!canvas) {
-      		alert('Error: I cannot find the canvas element!');
-      		return;
-  	  	}
+	  function init () {
+  	  // Find the canvas element.
+      canvas = document.getElementById('canvasApp');
+      if (!canvas) {
+      	alert('Error: I cannot find the canvas element!');
+      	return;
+  	  }
 
     	if (!canvas.getContext) {
-      		alert('Error: no canvas.getContext!');
-      		return;
+      	alert('Error: no canvas.getContext!');
+      	return;
     	}
 
     	// Get the 2D canvas context.
     	context = canvas.getContext('2d');
    	 	if (!context) {
-      		alert('Error: failed to getContext!');
-      		return;
+      	alert('Error: failed to getContext!');
+      	return;
     	}	
 
     	// Attach the mousemove event handler.
     	canvas.addEventListener('mousedown', ev_mousedown, false);
     	canvas.addEventListener('mouseup', ev_mouseup, false);
     	canvas.addEventListener('mousemove', ev_mousemove, false);
-	}
+	  }
 	
-	var x, y;
-	var paint = false;
-	var started = false;
-	function ev_mousedown(ev){
- 	 	paint = true;
-	}
-	function ev_mouseup(ev){
- 	 	paint = false;
- 	 	started = false;
-	}
+	  var x, y;
+	  var paint = false;
+	  var started = false;
+	  function ev_mousedown(ev){
+ 	 	  paint = true;
+	  }
+	  function ev_mouseup(ev){
+ 	 	  paint = false;
+ 	 	  started = false;
+	  }
 
+	  function ev_mousemove (ev) {
+		  if (paint){
+			  var w = window,
+        d = document,
+        e = d.documentElement,
+        g = d.getElementsByTagName('body')[0],
+        x2 = w.innerWidth || e.clientWidth || g.clientWidth,
+        y2 = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
-	function ev_mousemove (ev) {
-		if (paint)
-		{
-			var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x2 = w.innerWidth || e.clientWidth || g.clientWidth,
-    y2 = w.innerHeight|| e.clientHeight|| g.clientHeight;
-			// Get the mouse position relative to the canvas element.
-			//JOE switched method from layer to client, fixing the annoying starting line
-  			if (ev.layerX || ev.layerX == 0) { 
-    			x = ev.clientX;
-    			y = ev.clientY;
-  			}
+			  // Get the mouse position relative to the canvas element.
+			  //JOE switched method from layer to client, fixing the annoying starting line
+  		  if (ev.layerX || ev.layerX == 0) { 
+    		  x = ev.clientX;
+    		  y = ev.clientY;
+  		  }
 
-  			// The event handler works like a drawing pencil which tracks the mouse 
-  			// movements. We start drawing a path made up of lines.
-  			if (started==false) {
-  				context.moveTo(x, y);
-    			context.beginPath();
-    			started = true;
-  			} else {
-    			context.lineTo(x, y);
-  			  	context.stroke();
-  			}
-  		}
-	}
+ 			  // The event handler works like a drawing pencil which tracks the mouse 
+ 			  // movements. We start drawing a path made up of lines.
+ 			  if (started==false) {
+  			  context.moveTo(x, y);
+    		  context.beginPath();
+    		  started = true;
+  		  } else {
+    		  context.lineTo(x, y);
+  		 	  context.stroke();
+  		  }
+  	  }
+	  } 
 	
-	init();
-},false); }
+	  init();
+  },false); 
+
+}
