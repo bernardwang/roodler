cccd849b81ef961bfac48b01d4d9f8a99980c3e6
bw-vbnm <bwang19@illinois.edu>
2013-11-21 18:59:46 -0600

    added button function
    switches from picture (from flask) to a static canvas

--- app/static/js/doodle.js
+++ app/static/js/doodle.js
@@ -1,3 +1,6 @@
+
+//*************************************************************************************
+
 var canvas, context, paint = false,
     prevX = 0,
     currX = 0,
@@ -29,102 +32,27 @@ function init() {
 }
 
 function getCoords(e){
-        prevX = currX;
-        prevY = currY;
-        currX = e.pageX - canvas.offsetLeft - 50;
-        currY = e.pageY - canvas.offsetTop - 50;
-        draw();
+  prevX = currX; 
+  prevY = currY;
+  currX = e.pageX - canvas.offsetLeft - 50; //gets coords and fixes offset
+  currY = e.pageY - canvas.offsetTop - 50; 
+  draw(); 
 }
 
 function draw() {
   if(paint){
-    context.beginPath();
-    context.moveTo(prevX, prevY);
+    context.beginPath(); //starts drawing
+    context.moveTo(prevX, prevY);  //draws from prev coord to curr
     context.lineTo(currX, currY);
-    context.strokeStyle = color;
-    context.lineWidth = width;
-    context.stroke();
-    context.closePath();
+    context.strokeStyle = color; //sets color
+    context.lineWidth = width; //sets draw width
+    context.stroke(); //draws
+    context.closePath(); //stops drawing
   }
 }
 
-/*
-if(window.addEventListener) {
-
-  window.addEventListener('load', function () {
-	
-	  var canvas, context;
-	  var clearButton = document.getElementById["clear"];
-	
-	  function init () {
-  	  // Find the canvas element.
-      canvas = document.getElementById('canvasApp');
-      if (!canvas) {
-      	alert('Error: I cannot find the canvas element!');
-      	return;
-  	  }
-
-    	if (!canvas.getContext) {
-      	alert('Error: no canvas.getContext!');
-      	return;
-    	}
-
-    	// Get the 2D canvas context.
-    	context = canvas.getContext('2d');
-   	 	if (!context) {
-      	alert('Error: failed to getContext!');
-      	return;
-    	}	
-
-    	// Attach the mousemove event handler.
-    	canvas.addEventListener('mousedown', ev_mousedown, false);
-    	canvas.addEventListener('mouseup', ev_mouseup, false);
-    	canvas.addEventListener('mousemove', ev_mousemove, false);
-	  }
-	
-	  var x, y;
-	  var paint = false;
-	  var started = false;
-
-	  function ev_mousedown(ev){
- 	 	  paint = true;
-	  }
-	  function ev_mouseup(ev){
- 	 	  paint = false;
- 	 	  started = false;
-	  }
-
-	  function ev_mousemove (ev) {
-		  if (paint){
-			  var w = window,
-        d = document,
-        e = d.documentElement,
-        g = d.getElementsByTagName('body')[0],
-        x2 = w.innerWidth || e.clientWidth || g.clientWidth,
-        y2 = w.innerHeight|| e.clientHeight|| g.clientHeight;
-
-			  // Get the mouse position relative to the canvas element.
-			  //JOE switched method from layer to client, fixing the annoying starting line
-  		  if (ev.layerX || ev.layerX == 0) { 
-    		  x = ev.clientX;
-    		  y = ev.clientY;
-  		  }
-
- 			  // The event handler works like a drawing pencil which tracks the mouse 
- 			  // movements. We start drawing a path made up of lines.
- 			  if (started==false) {
-  			  context.moveTo(x, y);
-    		  context.beginPath();
-    		  started = true;
-  		  } else {
-    		  context.lineTo(x, y);
-  		 	  context.stroke();
-  		  }
-  	  }
-	  } 
-	
-	  init();
-  },false);
-
+//*************************************************************************************
+function drawButton(){
+  $('.doodle').css("display","none");
+  $('#canvas').css("display","block");
 }
-*/
