313f32d9c5e4190eeef75b5917f746e1f4403d8e
bw-vbnm <bwang19@illinois.edu>
2013-12-09 22:58:53 -0600

    attempt at saving all files

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import base64
 import re
 
+from time import gmtime, strftime
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,7 +21,8 @@ def submitImg():
     file.write(dataURL.decode('base64'))
   file.close()
 
-  with open("/home/roodler/Dropbox/roodler/doodle2.png", "w") as file:
+  var time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+  with open("/home/roodler/Dropbox/roodler/"+time+".png", "w") as file:
     file.write(dataURL.decode('base64'))
   file.close()
   return "stuff"
